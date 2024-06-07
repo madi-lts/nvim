af063b81a5161f6d99f1496bb4820df3715031f6
madi-lts <madi.j.anderson@gmail.com>
2024-06-07 14:05:15 -0400

    first commit

---
+++ lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- vim.g.python3_host_prog = "/home/madi/anaconda3/bin/python"
+
+-- vim.g.maplocalleader = "<shift>"
+-- vim.opt.timeoutlen = 1000
